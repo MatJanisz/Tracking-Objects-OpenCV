1570447011156574a9d517f583416345dcb7a75a
Ola2904 <knapinska.aleksandra@gmail.com>
2020-06-09 16:02:52 +0200

    updated videos

--- tracking_objects.py
+++ tracking_objects.py
@@ -2,12 +2,12 @@ import cv2
 
 ############### Tracker Types #####################
 
-tracker = cv2.TrackerBoosting_create()
+# tracker = cv2.TrackerBoosting_create()
 # tracker = cv2.TrackerMIL_create()
 # tracker = cv2.TrackerKCF_create()
 # tracker = cv2.TrackerTLD_create()
 # tracker = cv2.TrackerMedianFlow_create()
-# tracker = cv2.TrackerCSRT_create()
+tracker = cv2.TrackerCSRT_create()
 # tracker = cv2.TrackerMOSSE_create()
 
 ########################################################
