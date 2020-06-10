53ab320ac998c3a033a92b99e6f2703c1ec4c959
Ola2904 <knapinska.aleksandra@gmail.com>
2020-06-10 13:49:52 +0200

    added report

--- tracking_objects.py
+++ tracking_objects.py
@@ -4,10 +4,10 @@ import cv2
 
 # tracker = cv2.TrackerBoosting_create()
 # tracker = cv2.TrackerMIL_create()
-# tracker = cv2.TrackerKCF_create()
+tracker = cv2.TrackerKCF_create()
 # tracker = cv2.TrackerTLD_create()
 # tracker = cv2.TrackerMedianFlow_create()
-tracker = cv2.TrackerCSRT_create()
+# tracker = cv2.TrackerCSRT_create()
 # tracker = cv2.TrackerMOSSE_create()
 
 ########################################################
